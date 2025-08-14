ba25e699891f3d848e45771b55a4c8b7ee8758ca
SioModd <danatos.vega@mail.ru>
2025-08-14 20:03:04 +0300

    commit all

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -1,5 +1,5 @@
 def is_year_leap(year):
-    return 'True' if year % 4 == 0 else 'false'
+    return True if year % 4 == 0 else False
 
 year = int(input('Введите год '))
 result = is_year_leap(year)
--- lesson_02/lesson_2_task_3.py
+++ lesson_02/lesson_2_task_3.py
@@ -2,5 +2,4 @@ def square(a):
     return a*a
 
 a = int(round(float(input('Введите сторону квадрата '))))
-print(a)
 print('Площадь квадрата = ',square(a) )
